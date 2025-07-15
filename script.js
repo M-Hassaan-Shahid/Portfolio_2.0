b08824e5988e03cff2260a4b4192bc7c903072db
Muhammad Hassaan Shahid <2023cs79@student.uet.edu.pk>
2025-07-15 15:34:59 +0500

    .

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ themeToggle.addEventListener('click', () => {
 menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
+  
 });
 
 navLinks.forEach(link => {
@@ -21,4 +22,13 @@ navLinks.forEach(link => {
     navbar.classList.remove('active');
     menuIcon.classList.remove('bx-x');
   });
+});
+
+window.addEventListener('click', (e) => {
+  if (navbar.classList.contains('active') && 
+      !navbar.contains(e.target) && 
+      e.target !== menuIcon) {
+    navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+  }
 });
